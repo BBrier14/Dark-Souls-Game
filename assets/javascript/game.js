5f08e5c7e6a547b8c09967074d6251b8b7218891
Benjamin Brier <ben.brier14@gmail.com>
2019-09-03 13:16:25 -0400

    created on click events for images and two variables

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,34 @@
+//Variables needed for the game
+var counter = 0;
+var targetNumber = 50;
 
+
+//Target number required shown on screen to user
+$("#souls-required").text(targetNumber);
+
+
+
+//On click functions for my images
 $("#demon").on("click", function() {
-alert("You touched the demon!")
+    counter += 1;
+alert("You have collected  " + counter + " souls!")
 
 });
 
+$("#gundyr").on("click", function() {
+    counter += 1;
+alert("You have collected  " + counter + " souls!")
+    
+});
+
+$("#soul").on("click", function() {
+    counter += 1;
+alert("You have collected  " + counter + " souls!")
+        
+});
+
+$("#vordt").on("click", function() {
+    counter += 1;
+alert("You have collected  " + counter + " souls!")
+    
+});
