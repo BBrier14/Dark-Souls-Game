dc3e229fc7928e98a0e2e1b50a3f76967617b50d
Benjamin Brier <ben.brier14@gmail.com>
2019-09-04 12:44:41 -0400

    souls required now randomizes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,8 @@
 //Variables needed for the game
 var counter = 0;
-var targetNumber = 50;
-var targetNumberOptions = (50,75,100,125)
+var targetNumber = soulsRequired;
+var targetNumberOptions = [50,75,100,125]
+var soulsRequired = targetNumberOptions[Math.floor(Math.random()*targetNumberOptions.length)];
 var numberOptions = [5,10,15,20];
 var increment = numberOptions[Math.round(Math.random())];
 var wins = 0;
@@ -9,7 +10,7 @@ var losses = 0;
 
 
 //Target number required shown on screen to user
-$("#souls-required").text(targetNumber);
+$("#souls-required").text(soulsRequired);
 $("#souls-acquired").text(counter);
 
 for (var i = 0; i < numberOptions.length; i++) {
@@ -25,22 +26,26 @@ $(".boss-image").on("click", function() {
     bossValue = parseInt(bossValue);
     counter += bossValue;
     $("#souls-acquired").text(counter);
+    
 
-    if (counter === targetNumber) {
+    if (counter === soulsRequired) {
         // alert("You may enter Anor Lando!");
         counter = 0;
         wins++;
         $("#souls-acquired").text(counter);
         $("#keys-acquired").text(wins);
-        
+        soulsRequired = targetNumberOptions[Math.floor(Math.random()*targetNumberOptions.length)];
+        $("#souls-required").text(soulsRequired);
         }
         
-    else if (counter >= targetNumber) {
+    else if (counter >= soulsRequired) {
         // alert("You have been consumed by the Abyss!")
         counter = 0;
         losses++;
         $("#souls-acquired").text(counter);
         $("#humanity-lost").text(losses);
+        soulsRequired = targetNumberOptions[Math.floor(Math.random()*targetNumberOptions.length)];
+        $("#souls-required").text(soulsRequired);
         }
 
     if (wins > 9) {
@@ -56,36 +61,3 @@ $(".boss-image").on("click", function() {
 });
 
 
-//On click functions for my images
-// $("#demon").on("click", function() {
-//     counter += increment;
-// alert("You have collected  " + counter + " souls!")
-
-// });
-
-// $("#gundyr").on("click", function() {
-//     counter += increment;
-// alert("You have collected  " + counter + " souls!")
-    
-// });
-
-// $("#soul").on("click", function() {
-//     counter += increment;
-// alert("You have collected  " + counter + " souls!")
-        
-// });
-
-// $("#vordt").on("click", function() {
-//     counter += increment;
-// alert("You have collected  " + counter + " souls!")
-    
-// });
-
-//Win & Loss conditions
-// if (counter === targetNumber) {
-// alert("You may enter Anor Lando!");
-// }
-
-// else if (counter >= targetNumber) {
-// alert("You may not enter!")
-// }
